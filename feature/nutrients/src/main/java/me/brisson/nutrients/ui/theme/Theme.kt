b26feddac167ed684613b145dca5c0db1fdf8a50
Gabriel Brisson <74081378+GabrielBrisson@users.noreply.github.com>
2023-03-24 19:05:47 -0300

    Criando tema do módulo de nutrientes  (#2)
    * Finishing Nutrients Theme
    
    * Finishing Nutrients Theme
    
    * Adding missing colors

--- feature/nutrients/src/main/java/me/brisson/nutrients/ui/theme/Theme.kt
+++ feature/nutrients/src/main/java/me/brisson/nutrients/ui/theme/Theme.kt
@@ -1,15 +1,35 @@
 package me.brisson.nutrients.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
+private val DarkColorsPalette = darkColors(
+    primary = PrimaryDark,
+    onBackground = OnBackGroundDark,
+    background = BackGroundDark,
+    surface = SurfaceDark
+)
 
-// TODO: finish theme
+private val LightColorsPalette = lightColors(
+    primary = PrimaryLight,
+    onBackground = OnBackGroundLight,
+    background = BackGroundLight,
+    surface = SurfaceLight
+)
 
 @Composable
-fun NutrientsTheme(content: @Composable () -> Unit) {
+fun NutrientsTheme(darkTheme: Boolean = isSystemInDarkTheme(),content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorsPalette
+    } else{
+        LightColorsPalette
+    }
 
     MaterialTheme(
+        colors = colors,
         content = content
     )
 }
