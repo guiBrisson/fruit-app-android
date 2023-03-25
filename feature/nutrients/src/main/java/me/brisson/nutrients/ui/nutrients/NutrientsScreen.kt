674d750286c606a31054a4e5d3259cc230e29375
GabrielBrisson <gabrielbrisson02@gmail.com>
2023-03-25 00:13:18 -0300

    WIP Creating NutrientsScreen

--- feature/nutrients/src/main/java/me/brisson/nutrients/ui/nutrients/NutrientsScreen.kt
+++ feature/nutrients/src/main/java/me/brisson/nutrients/ui/nutrients/NutrientsScreen.kt
@@ -2,19 +2,21 @@ package me.brisson.nutrients.ui.nutrients
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import me.brisson.nutrients.ui.theme.NutrientsTheme
 
@@ -26,24 +28,53 @@ fun NutrientsScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background),
-        contentAlignment = Alignment.Center
-    ) {
-        if (uiState.loading) {
-            LinearProgressIndicator(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
-                color = MaterialTheme.colors.primary
+    Column(modifier = modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.background)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Menu,
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onBackground
+                )
+            }
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onBackground
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.padding(20.dp, bottom = 0.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Info,
+                contentDescription = null,
+                tint = MaterialTheme.colors.onBackground
+            )
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = "Flavinoides",
+                color = MaterialTheme.colors.onBackground,
+                fontWeight = FontWeight.Black,
+                fontSize = 24.sp
             )
         }
-
-
+        Text(
+            modifier = Modifier.padding(start = 20.dp, top = 0.dp),
+            text = "aliados da longevidade",
+            color = MaterialTheme.colors.onBackground.copy(alpha = 0.6f)
+        )
+        Text(text = "")
     }
-
 }
 
 @Preview(showBackground = true, name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_NO)
