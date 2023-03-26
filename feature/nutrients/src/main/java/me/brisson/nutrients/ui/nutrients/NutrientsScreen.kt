1fce0a8571e87cdfe8b3d72067aaa07ec6aeb7c7
GabrielBrisson <gabrielbrisson02@gmail.com>
2023-03-26 16:01:52 -0300

    Fixing NavigationDrawer

--- feature/nutrients/src/main/java/me/brisson/nutrients/ui/nutrients/NutrientsScreen.kt
+++ feature/nutrients/src/main/java/me/brisson/nutrients/ui/nutrients/NutrientsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.launch
 import me.brisson.nutrients.domain.model.Nutrient
 import me.brisson.nutrients.ui.theme.NutrientsTheme
 
@@ -44,7 +45,7 @@ fun NutrientsScreen(
 }
 
 @Composable
-fun NutrientsScreen(
+internal fun NutrientsScreen(
     modifier: Modifier = Modifier,
     nutrient: Nutrient,
     nutrientsList: List<Nutrient>,
@@ -53,6 +54,7 @@ fun NutrientsScreen(
 ) {
 
     val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
     Scaffold(scaffoldState = scaffoldState, drawerContent = {
         NutrientsSideBar(
@@ -73,7 +75,11 @@ fun NutrientsScreen(
                         .fillMaxWidth()
                         .padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.Rounded.Menu,
                             contentDescription = null,
@@ -132,12 +138,33 @@ fun NutrientsScreen(
 @Preview(showBackground = true, name = "Light", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewNutrientsScreenDark() {
-    NutrientsTheme {
-        NutrientsScreen(nutrient = Nutrient(
-            name = "Flavonoides", subTitle = "aliados da longevidade", paragraphs = listOf(
+    val nutrients = listOf<Nutrient>(
+        Nutrient(
+            id = 1,
+            name = "Flavonoides",
+            subTitle = "Melhor amigo dos Flávios",
+            paragraphs = listOf(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            )
+        ),
+        Nutrient(
+            id = 2,
+            name = "Vitamina C",
+            subTitle = "Melhor amigo dos velhos",
+            paragraphs = listOf(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
             )
-        ), onBack = { }, nutrientsList = emptyList(), onNutrientClick = {})
+        )
+    )
+    NutrientsTheme {
+        NutrientsScreen(
+            nutrient = nutrients[1],
+            onBack = { },
+            nutrientsList = nutrients,
+            onNutrientClick = {})
     }
 }
