ebbdea660eacc3dc2597af887f7b61719337b857
guiBrisson <guibrisson@hotmail.com>
2023-03-26 00:55:11 -0300

    AppButton

--- app/src/main/java/me/brisson/fruitapp/ui/MainNavigation.kt
+++ app/src/main/java/me/brisson/fruitapp/ui/MainNavigation.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import me.brisson.nutrients.NutrientsNavigationRoutes
 import me.brisson.nutrients.nutrientsNavigation
@@ -22,6 +21,5 @@ fun MainNavigation(
     ) {
         nutrientsNavigation(navController)
     }
-
 
 }
--- core/ui/src/main/java/me/brisson/ui/components/Button.kt
+++ core/ui/src/main/java/me/brisson/ui/components/Button.kt
@@ -3,10 +3,7 @@ package me.brisson.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -19,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import me.brisson.ui.theme.FruitAppTheme
 
@@ -31,22 +29,37 @@ fun AppButton(
         Icon(imageVector = Icons.Rounded.ArrowForward, contentDescription = null)
     },
     backgroundColor: Color = MaterialTheme.colors.background,
+    borderColor: Color = MaterialTheme.colors.onBackground,
     accentColor: Color = MaterialTheme.colors.onBackground,
+    offset: Dp = 2.dp
 ) {
-    Row(
-        modifier = modifier
-            .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(8.dp) )
-            .clip(RoundedCornerShape(8.dp))
-            .clickable { onClick?.let { it() } }
-            .padding(horizontal = 16.dp, vertical = 14.dp)
-            .background(backgroundColor),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        text()
-        trailingIcon()
+    val shape = RoundedCornerShape(8.dp)
+    Box {
+        Box(
+            modifier = modifier
+                .matchParentSize()
+                .offset(x = offset, y = offset)
+                .clip(shape)
+                .background(accentColor)
+        )
+        Row(
+            modifier = modifier
+                .height(52.dp)
+                .border(width = 1.dp, color =  borderColor, shape = shape)
+                .clip(shape)
+                .background(backgroundColor)
+                .clickable { onClick?.let { it() } }
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            text()
+            trailingIcon()
+        }
+
     }
 }
+
 
 @Preview
 @Composable
