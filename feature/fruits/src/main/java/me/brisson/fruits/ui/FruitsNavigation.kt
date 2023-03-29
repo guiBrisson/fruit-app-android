b10f5c0670a4195d75ef412ae547eea20ac08508
guiBrisson <guibrisson@hotmail.com>
2023-03-29 13:06:47 -0300

    Fruit screen

--- feature/fruits/src/main/java/me/brisson/fruits/ui/FruitsNavigation.kt
+++ feature/fruits/src/main/java/me/brisson/fruits/ui/FruitsNavigation.kt
@@ -3,6 +3,7 @@ package me.brisson.fruits.ui
 import androidx.navigation.*
 import androidx.navigation.compose.composable
 import me.brisson.fruits.ui.crop_season.CropSeasonScreen
+import me.brisson.fruits.ui.fruit.FruitScreen
 import me.brisson.fruits.ui.home.HomeScreen
 import me.brisson.fruits.ui.month.MonthScreen
 import me.brisson.fruits.ui.nutrient.NutrientScreen
@@ -13,11 +14,13 @@ object FruitsNavigationScreens {
     const val HOME_SCREEN = "home"
     const val NUTRIENT_SCREEN = "nutrient"
     const val CROP_SCREEN = "crop"
-    const val MONTH_SCREEN = "month_screen"
+    const val MONTH_SCREEN = "month"
+    const val FRUIT_SCREEN = "fruit"
 }
 
 object FruitsNavigationArgs {
     const val MONTH_NAME_ARGS = "month_name"
+    const val FRUIT_ID_ARGS = "fruit_id"
 }
 
 object FruitsNavigationRoutes {
@@ -27,6 +30,8 @@ object FruitsNavigationRoutes {
     const val CROP_ROUTE = FruitsNavigationScreens.CROP_SCREEN
     const val MONTH_ROUTE =
         "${FruitsNavigationScreens.MONTH_SCREEN}/{${FruitsNavigationArgs.MONTH_NAME_ARGS}}"
+    const val FRUIT_ROUTE =
+        "${FruitsNavigationScreens.FRUIT_SCREEN}/{${FruitsNavigationArgs.FRUIT_ID_ARGS}}"
 }
 
 fun NavGraphBuilder.fruitsNavigation(navController: NavController) {
@@ -38,6 +43,10 @@ fun NavGraphBuilder.fruitsNavigation(navController: NavController) {
 
         composable(route = FruitsNavigationRoutes.HOME_ROUTE) {
             HomeScreen(
+                onFruit = { fruitId ->
+                    val route = "${FruitsNavigationScreens.FRUIT_SCREEN}/$fruitId"
+                    navController.navigate(route)
+                },
                 onNutrients = { navController.navigate(FruitsNavigationRoutes.NUTRIENT_ROUTE) },
                 onCrops = { navController.navigate(route = FruitsNavigationRoutes.CROP_ROUTE) }
             )
@@ -67,9 +76,24 @@ fun NavGraphBuilder.fruitsNavigation(navController: NavController) {
             })
         ) {
             MonthScreen(
-                onFruit = { /* TODO */ },
+                onFruit = { fruitId ->
+                    val route = "${FruitsNavigationScreens.FRUIT_SCREEN}/$fruitId"
+                    navController.navigate(route)
+                },
                 onBack = { navController.navigateUp() }
             )
+        }
+
+        composable(
+            route = FruitsNavigationRoutes.FRUIT_ROUTE,
+            arguments = listOf(navArgument(FruitsNavigationArgs.FRUIT_ID_ARGS) {
+                type = NavType.LongType
+            })
+        ) {
+            FruitScreen(
+                onBack = { navController.navigateUp() }
+            )
+
         }
     }
 }
---
+++ feature/fruits/src/main/java/me/brisson/fruits/ui/fruit/FruitComponents.kt
@@ -0,0 +1,34 @@
+package me.brisson.fruits.ui.fruit
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun FruitIconButton(
+    modifier: Modifier = Modifier,
+    icon: @Composable () -> Unit = {
+        Icon(
+            tint = Color.White,
+            imageVector = Icons.Rounded.ArrowBackIosNew,
+            contentDescription = null
+        )
+    },
+    onClick: () -> Unit
+) {
+    IconButton(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(Color.Black.copy(alpha = 0.4f)),
+        onClick = onClick
+    ) {
+        icon()
+    }
+}
