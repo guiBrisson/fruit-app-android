fc01797e6d63561dc5d75631662a6348787f7f89
guiBrisson <guibrisson@hotmail.com>
2023-03-31 20:56:20 -0300

    Passing recipe id as nav args

--- feature/recipes/src/main/java/me/brisson/recipes/RecipeNavigation.kt
+++ feature/recipes/src/main/java/me/brisson/recipes/RecipeNavigation.kt
@@ -4,28 +4,26 @@ import androidx.navigation.*
 import androidx.navigation.compose.composable
 import me.brisson.recipes.ui.recipe.RecipeScreen
 
+object RecipeNavigationScreen {
+    const val RECIPE_SCREEN = "recipe"
+}
+
 object RecipeNavigationRoutes {
-    const val RECIPE_ROUTE = "recipe"
-    const val RECIPE_DETAIL_ROUTE = "recipe_detail"
+    const val RECIPE_ROUTE =
+        "${RecipeNavigationScreen.RECIPE_SCREEN}/{${RecipeNavigationArgs.RECIPE_ID_ARGS}}"
 }
 
 object RecipeNavigationArgs {
     const val RECIPE_ID_ARGS = "recipe_id"
 }
 
-// todo: ver como vai passar o id
 fun NavGraphBuilder.recipeNavigation(navController: NavController) {
-    navigation(
-        startDestination = RecipeNavigationRoutes.RECIPE_DETAIL_ROUTE,
-        route = RecipeNavigationRoutes.RECIPE_ROUTE
+    composable(
+        route = RecipeNavigationRoutes.RECIPE_ROUTE,
+        arguments = listOf(navArgument(RecipeNavigationArgs.RECIPE_ID_ARGS) {
+            type = NavType.LongType
+        })
     ) {
-        composable(
-            route = RecipeNavigationRoutes.RECIPE_DETAIL_ROUTE,
-//            arguments = listOf(navArgument(RecipeNavigationArgs.RECIPE_ID_ARGS) {
-//                type = NavType.LongType
-//            })
-        ) {
-            RecipeScreen(onBack = { navController.navigateUp() })
-        }
+        RecipeScreen(onBack = { navController.navigateUp() })
     }
 }
--- feature/recipes/src/main/java/me/brisson/recipes/ui/recipe/RecipeUiState.kt
+++ feature/recipes/src/main/java/me/brisson/recipes/ui/recipe/RecipeUiState.kt
@@ -3,9 +3,12 @@ package me.brisson.recipes.ui.recipe
 import me.brisson.domain.model.Recipe
 
 data class RecipeUiState(
+    val missingRecipeId: Boolean = false,
+
     val recipe: Recipe? = null,
     val loading: Boolean = true,
     val recipeError: Throwable? = null,
+
     val selectedTab: RecipeTab = RecipeTab.Ingredient()
 )
 
