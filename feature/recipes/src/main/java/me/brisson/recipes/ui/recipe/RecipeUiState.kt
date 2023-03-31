9acaff53369b3cfafa1404c30933eb10f076c0f0
guiBrisson <guibrisson@hotmail.com>
2023-03-31 16:53:57 -0300

    RecipeScreen and components

--- feature/recipes/src/main/java/me/brisson/recipes/ui/recipe/RecipeUiState.kt
+++ feature/recipes/src/main/java/me/brisson/recipes/ui/recipe/RecipeUiState.kt
@@ -5,5 +5,11 @@ import me.brisson.recipes.domain.model.Recipe
 data class RecipeUiState(
     val recipe: Recipe? = null,
     val loading: Boolean = true,
-    val recipeError: Throwable? = null
+    val recipeError: Throwable? = null,
+    val selectedTab: RecipeTab = RecipeTab.Ingredient()
 )
+
+sealed class RecipeTab(open val name: String) {
+    data class Ingredient(override val name: String = "Ingredientes"): RecipeTab(name)
+    data class Instructions(override val name: String = "Modo de preparo"): RecipeTab(name)
+}
